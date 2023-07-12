fbe4daf69b32e674bf65df7e539eefb02c2bf3c6
Surya101010 <113287685+Surya101010@users.noreply.github.com>
2023-07-12 09:33:04 +0530

    Create isfactorial.java

---
+++ isfactorial.java
@@ -0,0 +1,9 @@
+public int nFactorial(int input1){
+        if(input1==0 || input1==1){
+            return 1;
+        }
+        else{
+            return input1*nFactorial(input1-1);
+        }
+	}
+}

008ed668185694222efc1a442e49989d643947ab
Surya101010 <113287685+Surya101010@users.noreply.github.com>
2023-07-17 10:10:04 +0530

    Create totalhill.java

---
+++ totalhill.java
@@ -0,0 +1,10 @@
+public int totalhill(int input1,int input2,int input3){
+    int sum=input2;
+    int temp=0;
+    int t=0;
+    for(int i=0;i<input1;i++){
+        temp=input2 + input3*i;
+        t+=temp*(i+1);
+    }
+        return t;
+    }

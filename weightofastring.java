f846f4433f558a45185cf6420d6caddbd0461091
Surya101010 <113287685+Surya101010@users.noreply.github.com>
2023-07-18 11:42:56 +0530

    Create weightofastring.java

---
+++ weightofastring.java
@@ -0,0 +1,32 @@
+public int weightofastring(String input1,String input2){
+  String small="abcdefghijklmnopqrstuvwxyz";  
+  int sum=0,i;  
+  for(i=0;i<input1.length();i++)  
+    { 
+      if(input2==0)    
+      {      
+        char c=input1.charAt(i);
+        if(Character.isUpperCase(c)) 
+          c=Character.toLowerCase(c);  
+        if(c!='a'&&c!='e'&&c!='i'&&c!='o'&&c!='u')     
+        {             int index=small.indexOf(c);   
+         if(index>=0)          
+           sum+=index+1;   
+        }           
+        else  
+          sum+=0; 
+      }          
+      else         
+      {             
+        char c=input1.charAt(i);        
+        if(Character.isUpperCase(c))       
+          c=Character.toLowerCase(c);               
+          int index=small.indexOf(c);    
+        if(index>=0)    
+          sum+=index+1;  
+        else              
+          sum+=0;           
+      }
+    }        
+  return sum;   
+  } 
